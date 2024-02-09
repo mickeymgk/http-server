7c93c9996e20d07a0582b6f76cd78d282933ab8c
mickeymgk <35106419+mickeymgk@users.noreply.github.com>
2024-02-09 21:59:03 +0300

    pass stage 6

--- app/server.c
+++ app/server.c
@@ -6,8 +6,54 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
+
+void *handle_connection(void *arg) {
+    int client_socket = *((int *)arg);
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received < 0) {
+        printf("Error receiving data: %s\n", strerror(errno));
+        close(client_socket);
+        pthread_exit(NULL);
+    }
+
+    char *buffer_copy = strdup(buffer);
+    char *path = strtok(buffer, " ");
+    path = strtok(NULL, " ");
+
+    if (strcmp(path, "/user-agent") == 0) {
+        char *user_agent = strstr(buffer_copy, "User-Agent: ");
+        if (user_agent != NULL) {
+            user_agent += strlen("User-Agent: ");
+            char *end_of_user_agent = strstr(user_agent, "\r\n");
+            if (end_of_user_agent != NULL) {
+                *end_of_user_agent = '\0';
+                const char *response_format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
+                char response[BUFFER_SIZE];
+                sprintf(response, response_format, strlen(user_agent), user_agent);
+                send(client_socket, response, strlen(response), 0);
+            }
+        }
+        free(buffer_copy); // why not free duplicates?
+    } else if (strncmp(path, "/echo/", 6) == 0) {
+        char *echo_string = path + 6;
+        char response[1024];
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", strlen(echo_string), echo_string);
+        send(client_socket, response, strlen(response), 0);
+    } else if (strcmp(path, "/") == 0) {
+        const char *response = "HTTP/1.1 200 OK\r\n\r\n";
+        send(client_socket, response, strlen(response), 0);
+    } else {
+        const char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        send(client_socket, response, strlen(response), 0);
+    }
+
+    close(client_socket);
+    pthread_exit(NULL);
+}
 
 int main() {
     // Disable output buffering
@@ -54,51 +100,18 @@ int main() {
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
 
-    int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-    printf("Client connected\n");
+    while (1) {
+        int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+        printf("Client connected\n");
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-    if (bytes_received < 0) {
-        printf("Error receiving data: %s\n", strerror(errno));
-        close(client_socket);
-        close(server_fd);
-        return 1;
-    }
-
-    char *buffer_copy = strdup(buffer);
-    char *path = strtok(buffer, " ");
-    path = strtok(NULL, " ");
-
-    if (strcmp(path, "/user-agent") == 0) {
-        printf("UA BUFFER:\n%s\n", buffer_copy);
-        char *user_agent = strstr(buffer_copy, "User-Agent: ");
-        if (user_agent != NULL) {
-            user_agent += strlen("User-Agent: ");
-            char *end_of_user_agent = strstr(user_agent, "\r\n");
-            if (end_of_user_agent != NULL) {
-                *end_of_user_agent = '\0';
-                const char *response_format = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s";
-                char response[BUFFER_SIZE];
-                sprintf(response, response_format, strlen(user_agent), user_agent);
-                send(client_socket, response, strlen(response), 0);
-            }
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handle_connection, (void *)&client_socket) != 0) {
+            printf("Failed to create thread\n");
+            close(client_socket);
+        } else {
+            pthread_detach(thread);
         }
-        free(buffer_copy); // why not free duplicates?
-    } else if (strncmp(path, "/echo/", 6) == 0) {
-        char *echo_string = path + 6;
-        char response[1024];
-        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", strlen(echo_string), echo_string);
-        send(client_socket, response, strlen(response), 0);
-    } else if (strcmp(path, "/") == 0) {
-        const char *response = "HTTP/1.1 200 OK\r\n\r\n";
-        send(client_socket, response, strlen(response), 0);
-    } else {
-        const char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        send(client_socket, response, strlen(response), 0);
     }
-
-    close(client_socket);
 
     close(server_fd);
 
