30f7cf7ed6cb9eae81de3bb4eb838aa131b3c03f
mickeymgk <35106419+mickeymgk@users.noreply.github.com>
2024-02-09 17:54:26 +0300

    pass stage 3

--- app/server.c
+++ app/server.c
@@ -7,58 +7,79 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
+   // Disable output buffering
+    setbuf(stdout, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    printf("Logs from your program will appear here!\n");
 
-	// Uncomment this block to pass the first stage
-	//
-	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
-	
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-	
-	// Since the tester restarts your program quite often, setting REUSE_PORT
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
-	
-	int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
+    // Uncomment this block to pass the first stage
+    //
+    int server_fd, client_addr_len;
+    struct sockaddr_in client_addr;
 
-    const char *response = "HTTP/1.1 200 OK\r\n\r\n";
-    send(client_socket, response, strlen(response), 0);
-	
-	close(server_fd);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        printf("Socket creation failed: %s...\n", strerror(errno));
+        return 1;
+    }
 
-	return 0;
+    // Since the tester restarts your program quite often, setting REUSE_PORT
+    // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+        printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    struct sockaddr_in serv_addr = {.sin_family = AF_INET,
+                                    .sin_port = htons(4221),
+                                    .sin_addr = {htonl(INADDR_ANY)},
+    };
+
+    if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+        printf("Bind failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        printf("Listen failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    printf("Waiting for a client to connect...\n");
+    client_addr_len = sizeof(client_addr);
+
+    int client_socket = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    printf("Client connected\n");
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received < 0) {
+        printf("Error receiving data: %s\n", strerror(errno));
+        close(client_socket);
+        close(server_fd);
+        return 1;
+    }
+
+    char *path = strtok(buffer, " ");
+    path = strtok(NULL, " ");
+
+    if (strcmp(path, "/") == 0) {
+        const char *response = "HTTP/1.1 200 OK\r\n\r\n";
+        send(client_socket, response, strlen(response), 0);
+    } else {
+        const char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        send(client_socket, response, strlen(response), 0);
+    }
+
+    close(client_socket);
+
+    close(server_fd);
+
+    return 0;
 }
